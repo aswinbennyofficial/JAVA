ae15310bc94fab031896221b834a76c1ff38f133
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-02-24 19:28:33 +0530

    Update product_table_of_prime-number.java

--- Loops/product_table_of_prime-number.java
+++ Loops/product_table_of_prime-number.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Main{
     public static void main(String args[]){
-        int s,e,i,j,factCount=0;
+        int s,e,i,j,isPrime=1;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the starting number: ");
         s=sc.nextInt();
@@ -9,14 +9,15 @@ class Main{
         e=sc.nextInt();
 
         for (i=s;i<=e;i++){
-            factCount=0;
-            for(j=1;j<=i/2;j++){
+            isPrime=1;
+            for(j=2;j<=i/2;j++){
                 if (i%j==0){
-                    factCount++;
+                    isPrime=0;
+                    break;
                 }
             }
 
-            if(factCount<2 && (i!=1 && i!=0) ){
+            if(isPrime==1 && (i!=1 && i!=0) ){
                 for(j=1;j<=10;j++){
                     System.out.format("%d X %d = %d\n",i,j,j*i);
                 }
