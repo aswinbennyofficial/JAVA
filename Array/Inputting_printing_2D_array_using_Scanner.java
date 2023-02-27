5664e3a3b7b052cd6401e69105a58d789aa624da
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-02-27 06:23:03 +0530

    Create Inputting_printing_2D_array_using_Scanner.java

---
+++ Array/Inputting_printing_2D_array_using_Scanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rl=sc.nextInt();
+        int cl= sc.nextInt();
+        int i,j;
+        int arr[][]=new int[rl][cl];
+
+        for (i=0;i<rl;i++){
+            for (j=0;j<cl;j++){
+            arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for (i=0;i<rl;i++){
+            for (j=0;j<cl;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
