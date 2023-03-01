e8867572bf65ea6ef6a8e239f52975c15b85febe
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-01 06:19:41 +0530

    Create Remove_even_in_arraylist.java

---
+++ ArrayList/Remove_even_in_arraylist.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+    ArrayList<Integer> t1=new ArrayList<>();
+       Scanner sc=new Scanner(System.in);
+       int i;
+       for(i=0;i<=6;i++){
+           t1.add(sc.nextInt());
+       }
+        System.out.println("inserted array list: "+t1);
+
+       for(i=t1.size()-1;i>=0;i--){
+           if(t1.get(i)%2==0){
+               t1.remove(i);
+           }
+       }
+        System.out.println("output arraylist: "+t1);
+    }
+}
