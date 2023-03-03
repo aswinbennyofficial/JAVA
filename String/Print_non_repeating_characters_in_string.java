15b4312aef3c803e2ad012a25076decad489cb24
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-03 06:55:38 +0530

    Create Print_non_repeating_characters_in_string.java

---
+++ String/Print_non_repeating_characters_in_string.java
@@ -0,0 +1,25 @@
+public class Prog {
+    public static void main(String[] args) {
+        String str="aabbbcddfeeeg";
+        char[] arr=str.toCharArray();
+        str="";
+        int i;
+
+        for(i=0;i< arr.length;i++){
+            if (i< arr.length-1 && arr[i]==arr[i+1]){
+                continue;
+            }
+            else if(i>0 && arr[i]==arr[i-1]){
+                continue;
+            }
+            else{
+                str+=arr[i];
+            }
+
+
+        }
+
+        System.out.println(str);
+    }
+
+}
