4f80a503a07d1759e1e2c2fa06f7bc7e6feab936
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-06 06:18:52 +0530

    Create Bubble_sort.java

---
+++ Sorting/Bubble_sort.java
@@ -0,0 +1,20 @@
+// make last element larger
+
+class Main{
+    public static void main(String[] args) {
+        int arr[]={1,5,1,-1,7,4,8,-5};
+        int i,j;
+        for(i=0;i< arr.length;i++){
+            for(j=0;j< arr.length-1-i;j++){
+                if(arr[j]>arr[j+1] && arr[j]!=arr[j+1]){
+                    arr[j]=arr[j]^arr[j+1];
+                    arr[j+1]=arr[j]^arr[j+1];
+                    arr[j]=arr[j]^arr[j+1];
+                }
+            }
+        }
+        for(i=0;i< arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
