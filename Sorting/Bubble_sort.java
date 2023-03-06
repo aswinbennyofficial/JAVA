d5bba0ced6d1b7e171ad2ec71043715fb349726a
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-06 06:21:03 +0530

    Update Bubble_sort.java

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,10 +1,8 @@
-// make last element larger
-
 class Main{
     public static void main(String[] args) {
         int arr[]={1,5,1,-1,7,4,8,-5};
         int i,j;
-        for(i=0;i< arr.length;i++){
+        for(i=0;i< arr.length-1;i++){
             for(j=0;j< arr.length-1-i;j++){
                 if(arr[j]>arr[j+1] && arr[j]!=arr[j+1]){
                     arr[j]=arr[j]^arr[j+1];
