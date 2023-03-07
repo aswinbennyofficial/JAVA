841de3255599dadaa0486f03b63c02fe2d9cdb1e
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-07 06:30:10 +0530

    Update Searching_in_array.java

--- Recursion/Searching_in_array.java
+++ Recursion/Searching_in_array.java
@@ -16,3 +16,30 @@ class Main{
         num(arr, n,0);
     }
 }
+
+/*
+or
+
+class Main{
+    public static int num(int arr[],int n,int i){
+        if (i==arr.length){
+            //System.out.println("Not found");
+            return -1;
+        }
+        else if(arr[i]==n){
+           // System.out.println(i);
+            return i;
+        }
+         return num(arr,n,i+1);
+        //return i;
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5,6,7,8,9};
+       int n=10;
+        System.out.println(num(arr, n,0));
+    }
+}
+
+
+
+*/
