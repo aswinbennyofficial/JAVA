6426720131b136f8c0314737bc4674c2e21cc3ce
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-07 06:15:06 +0530

    Create Searching_in_array.java

---
+++ Recursion/Searching_in_array.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void num(int arr[],int n,int i){
+        if (i==arr.length){
+            System.out.println("Not found");
+            return;
+        }
+        else if(arr[i]==n){
+            System.out.println(i);
+            return;
+        }
+         num(arr,n,i+1);
+    }
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5,6,7,8,9};
+       int n=8;
+        num(arr, n,0);
+    }
+}
