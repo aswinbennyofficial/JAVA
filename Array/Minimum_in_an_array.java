9b02f71993e1885b79ff629709ae1347955ddff1
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-21 19:35:29 +0530

    Create Minimum_in_an_array.java

---
+++ Array/Minimum_in_an_array.java
@@ -0,0 +1,24 @@
+
+
+class Prog{
+
+    static int operation(int arr[]){
+        int min=arr[0],i;
+
+        for(i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={-1,1,2,3,4,5,6,7};
+        int i;
+
+        System.out.println(operation(arr));
+
+    }
+}
