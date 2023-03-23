d1eada8680a23e4ebdcd454ac784fee9b5b5a037
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-23 22:44:54 +0530

    Create Length_of_LinkedList.java

---
+++ LinkedList/Length_of_LinkedList.java
@@ -0,0 +1,44 @@
+class Main{
+
+    private ListNode head;
+
+    private static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+    // length of linkedlist
+    public void length(){
+        int count=0;
+        ListNode current=head;
+        while (current!=null){
+            count++;
+            current=current.next;
+        }
+        System.out.println(count);
+    }
+
+
+    public static void main(String[] args) {
+        Main sll=new Main(); // to have head inside main
+        sll.head=new ListNode(10);
+
+        ListNode n2=new ListNode(11);
+        ListNode n3=new ListNode(12);
+        ListNode n4=new ListNode(13);
+
+        //connect nodes together
+        sll.head.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+
+       sll.length();
+
+    }
+
+}
