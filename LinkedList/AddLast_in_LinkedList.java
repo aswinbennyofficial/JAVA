ca0deda06eab075daf1c58c58c7ae043d912421e
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-24 18:20:53 +0530

    Create AddLast_in_LinkedList.java

---
+++ LinkedList/AddLast_in_LinkedList.java
@@ -0,0 +1,62 @@
+import java.security.PublicKey;
+
+class Main{
+
+    private ListNode head;
+
+    private static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+    public void addLast(int data){
+        ListNode newNode=new ListNode(data);
+        if(head.next==null){
+            head=newNode;
+            return;
+        }
+        ListNode current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=newNode;
+
+    }
+
+    public void display(){
+        ListNode current=head;
+        while (current!=null){
+            System.out.print(current.data+" -> ");
+            current=current.next;
+        }
+        System.out.println(" Null");
+    }
+
+
+    public static void main(String[] args) {
+        Main sll=new Main(); // to have head inside main
+        sll.head=new ListNode(10);
+
+        ListNode n2=new ListNode(11);
+        ListNode n3=new ListNode(12);
+        ListNode n4=new ListNode(13);
+
+        //connect nodes together
+        sll.head.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+
+       sll.addLast(14);
+       sll.addLast(15);
+       sll.addLast(16);
+       sll.display();
+
+    }
+
+}
