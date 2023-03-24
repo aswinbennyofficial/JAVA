6c29c047fbcff52ae2be67753704b1f6dddded1a
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-24 21:03:56 +0530

    Create Delete_Last_Node_in_LinkedList.java

---
+++ LinkedList/Delete_Last_Node_in_LinkedList.java
@@ -0,0 +1,62 @@
+
+class Main{
+
+    private ListNode head;
+
+    private static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+    public void delLast(){
+        int size=0,i;
+        ListNode current=head;
+        while(current.next!=null){
+            current=current.next;
+            size++;
+        }
+        current=head;
+        for(i=1;i<size;i++){
+            current=current.next;
+        }
+        current.next=null;
+        //return current;
+    }
+
+
+    public void display(){
+        ListNode current=head;
+        while (current!=null){
+            System.out.print(current.data+" -> ");
+            current=current.next;
+        }
+        System.out.println(" Null");
+    }
+
+
+    public static void main(String[] args) {
+        Main sll=new Main(); // to have head inside main
+        sll.head=new ListNode(10);
+
+        ListNode n2=new ListNode(11);
+        ListNode n3=new ListNode(12);
+        ListNode n4=new ListNode(13);
+
+        //connect nodes together
+        sll.head.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+
+        //System.out.println(sll.delLast().data);
+       sll.delLast();
+       sll.display();
+
+    }
+
+}
