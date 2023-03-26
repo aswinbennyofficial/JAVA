22af78266b2dabab475f79af4947f5c8f9e3dd6c
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-26 21:47:29 +0530

    Create Remove_Duplicate_From_Sorted_LinkedList.java

---
+++ LinkedList/Remove_Duplicate_From_Sorted_LinkedList.java
@@ -0,0 +1,64 @@
+
+class Main{
+
+    private ListNode head;
+
+    private static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+
+    public void removeDuplicate(){
+        ListNode current=head;
+
+        while(current.next!=null && current!=null){
+            if(current.data==current.next.data){
+                current.next=current.next.next;
+            }
+            else{
+                current=current.next;
+            }
+
+        }
+    }
+
+
+    public void display(){
+        ListNode current=head;
+        while (current!=null){
+            System.out.print(current.data+" -> ");
+            current=current.next;
+        }
+        System.out.println(" Null");
+    }
+
+
+    public static void main(String[] args) {
+        Main sll=new Main(); // to have head inside main
+        sll.head=new ListNode(10);
+
+        ListNode n2=new ListNode(12);
+        ListNode n3=new ListNode(12);
+        ListNode n4=new ListNode(12);
+        ListNode n5=new ListNode(14);
+
+        //connect nodes together
+        sll.head.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        n4.next=n5;
+
+
+        sll.removeDuplicate();
+        sll.display();
+
+    }
+
+}
