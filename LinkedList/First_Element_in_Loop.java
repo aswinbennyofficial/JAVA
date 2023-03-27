f52034846e21c7c71299b9a9cf96244faa43913d
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-03-27 21:44:19 +0530

    Create First_Element_in_Loop.java

---
+++ LinkedList/First_Element_in_Loop.java
@@ -0,0 +1,69 @@
+class Main{
+
+    private ListNode head;
+
+    private static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+    public void detectLoop(){
+        ListNode slowpointer=head;
+        ListNode fastpointer=head;
+
+        while(fastpointer.next!=null && fastpointer.next.next!=null){
+
+            slowpointer=slowpointer.next;
+            fastpointer=fastpointer.next.next;
+
+            if(slowpointer==fastpointer){
+                break;
+            }
+        }
+        slowpointer=head;
+        while(slowpointer!=fastpointer){
+            slowpointer=slowpointer.next;
+            fastpointer=fastpointer.next;
+        }
+
+        System.out.println(slowpointer.data);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Main sll=new Main(); // to have head inside main
+        sll.head=new ListNode(10);
+
+        ListNode n2=new ListNode(11);
+        ListNode n3=new ListNode(12);
+        ListNode n4=new ListNode(14);
+        ListNode n5=new ListNode(15);
+        ListNode n6=new ListNode(16);
+        ListNode n7=new ListNode(17);
+        ListNode n8=new ListNode(18);
+
+
+        sll.head.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        n4.next=n5;
+        n5.next=n6;
+        n6.next=n7;
+        n7.next=n8;
+        n8.next=n5;
+
+
+        sll.detectLoop();
+       
+
+    }
+
+}
